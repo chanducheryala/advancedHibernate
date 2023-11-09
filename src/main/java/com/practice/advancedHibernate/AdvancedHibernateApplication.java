4eb47de28388a04518384ed9a0e39ac80feb4f5a
chandu <cheryalachandu33@gmail.com>
2023-11-09 16:24:02 +0530

    initial commit

---
+++ src/main/java/com/practice/advancedHibernate/AdvancedHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.practice.advancedHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/advancedHibernate/AdvancedHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.advancedHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
